cd3b2c20187a2ddda04b16670a7aa52eee0c6d37
SECVBulRep <bfarukshin@gmail.com>
2024-12-22 19:18:50 +0300

    * Value task

---
+++ MemoryIssues/09.ValueTask/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+
+// AsyncTaskMethodBuilder<string> asyncTaskMethodBuilder = new AsyncTaskMethodBuilder<string>();
+//
+// asyncTaskMethodBuilder.SetResult();
+
+// async Task<string> ReadFileAsync(string filename)
+// {
+//     if (!File.Exists(filename))
+//         return string.Empty;
+//     return await File.ReadAllTextAsync(filename);
+// }
+
+
+// AsyncValueTaskMethodBuilder<string> asyncValueTaskMethodBuilder = new AsyncValueTaskMethodBuilder<string>();
+// asyncValueTaskMethodBuilder.SetResult();
+//
+// async ValueTask<string> ReadFileAsync2(string filename)
+// {
+//     if (!File.Exists(filename))
+//         return string.Empty;
+//     return await File.ReadAllTextAsync(filename);
+// }
+
+
+
+[SimpleJob]
+[MemoryDiagnoser]
+public class TaskVsValueTaskBenchmark
+{
+   
+    public Task<int> GetTaskAsync()
+    {
+        return Task.FromResult(42); 
+    }
+
+   
+    public ValueTask<int> GetValueTaskAsync()
+    {
+        return new ValueTask<int>(42); 
+    }
+
+    
+    [Benchmark]
+    public async Task<int> BenchmarkTask()
+    {
+        return await GetTaskAsync();
+    }
+
+    
+    [Benchmark]
+    public async ValueTask<int> BenchmarkValueTask()
+    {
+        return await GetValueTaskAsync();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<TaskVsValueTaskBenchmark>();
+    }
+}
