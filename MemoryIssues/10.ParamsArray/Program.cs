4a11fedaf2789aae14c6f169224aad75dbd363ce
bulat <farukshin@molot.ru>
2024-12-24 10:43:41 +0300

    * массив парамептров

---
+++ MemoryIssues/10.ParamsArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+[SimpleJob]
+[MemoryDiagnoser]
+public class ParamsAllocationBenchmark
+{
+    [Params(1, 5, 10)]
+    public int Count;
+
+    [Benchmark]
+    public int WithParamsArray()
+    {
+        return SumWithParams(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+
+    [Benchmark]
+    public int WithOverload()
+    {
+        return SumOverload(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+
+    private int SumWithParams(params int[] numbers)
+    {
+        int sum = 0;
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+    private int SumOverload(int n1, int n2, int n3, int n4, int n5, int n6, int n7, int n8, int n9, int n10)
+    {
+        return n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<ParamsAllocationBenchmark>();
+    }
+}
